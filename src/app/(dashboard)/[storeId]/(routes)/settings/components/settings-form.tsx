4d8a9146e2c715b1b58da0052fb87b251ea2dae1
Daniel Junior <oidanieljunior@gmail.com>
2023-07-01 23:38:51 -0300

    feat: add dashboard settings

---
+++ src/app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -0,0 +1,141 @@
+"use client";
+
+import { useState } from "react";
+
+import * as z from "zod";
+import ky from "ky";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Trash } from "lucide-react";
+import { Store } from "@prisma/client";
+import { useParams, useRouter } from "next/navigation";
+
+import { toast } from "@/lib/toast";
+import { Input } from "@/components/ui/input";
+import { useOrigin } from "@/hooks/use-origin";
+import { Button } from "@/components/ui/button";
+import { Heading } from "@/components/ui/heading";
+import { ApiAlert } from "@/components/ui/api-alert";
+import { Separator } from "@/components/ui/separator";
+import { AlertModal } from "@/components/modals/alert-modal";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+const formSchema = z.object({
+  name: z.string().min(2),
+});
+
+type SettingsFormValues = z.infer<typeof formSchema>;
+
+interface SettingsFormProps {
+  initialData: Store;
+}
+
+export const SettingsForm = ({ initialData }: SettingsFormProps) => {
+  const params = useParams();
+  const router = useRouter();
+  const origin = useOrigin();
+
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const form = useForm<SettingsFormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData,
+  });
+
+  const onSubmit = async (data: SettingsFormValues) => {
+    try {
+      setLoading(true);
+      await ky.patch(`/api/stores/${params.storeId}`, { json: data });
+      router.refresh();
+      toast.success("Store updated.");
+    } catch (error: any) {
+      toast.error("Something went wrong.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const onDelete = async () => {
+    try {
+      setLoading(true);
+      await ky.delete(`/api/stores/${params.storeId}`);
+      router.refresh();
+      router.push("/");
+      toast.success("Store deleted.");
+    } catch (error: any) {
+      toast.error("Make sure you removed all products and categories first.");
+    } finally {
+      setLoading(false);
+      setOpen(false);
+    }
+  };
+
+  return (
+    <>
+      <AlertModal
+        isOpen={open}
+        onClose={() => setOpen(false)}
+        onConfirm={onDelete}
+        loading={loading}
+      />
+      <div className="flex items-center justify-between">
+        <Heading
+          title="Store settings"
+          description="Manage store preferences"
+        />
+        <Button
+          disabled={loading}
+          variant="destructive"
+          size="sm"
+          onClick={() => setOpen(true)}
+        >
+          <Trash className="h-4 w-4" />
+        </Button>
+      </div>
+      <Separator />
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="w-full space-y-8"
+        >
+          <div className="grid grid-cols-3 gap-8">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={loading}
+                      placeholder="Store name"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button disabled={loading} className="ml-auto" type="submit">
+            Save changes
+          </Button>
+        </form>
+      </Form>
+      <Separator />
+      <ApiAlert
+        title="NEXT_PUBLIC_API_URL"
+        variant="public"
+        description={`${origin}/api/${params.storeId}`}
+      />
+    </>
+  );
+};
---
+++ src/app/api/stores/[storeId]/route.ts
@@ -0,0 +1,72 @@
+import { NextRequest, NextResponse } from "next/server";
+import { auth } from "@clerk/nextjs";
+
+import { db } from "@/lib/db";
+
+export async function PATCH(
+  req: NextRequest,
+  { params }: { params: { storeId: string } }
+) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+
+    const { name } = body;
+
+    if (!userId) {
+      return new NextResponse("Unauthenticated", { status: 403 });
+    }
+
+    if (!name) {
+      return new NextResponse("Name is required", { status: 400 });
+    }
+
+    if (!params.storeId) {
+      return new NextResponse("Store id is required", { status: 400 });
+    }
+
+    const store = await db.store.updateMany({
+      where: {
+        id: params.storeId,
+        userId,
+      },
+      data: {
+        name,
+      },
+    });
+
+    return NextResponse.json(store);
+  } catch (e) {
+    const error = e as Error;
+    return new NextResponse(error.message, { status: 500 });
+  }
+}
+
+export async function DELETE(
+  req: NextRequest,
+  { params }: { params: { storeId: string } }
+) {
+  try {
+    const { userId } = auth();
+
+    if (!userId) {
+      return new NextResponse("Unauthenticated", { status: 403 });
+    }
+
+    if (!params.storeId) {
+      return new NextResponse("Store id is required", { status: 400 });
+    }
+
+    const store = await db.store.deleteMany({
+      where: {
+        id: params.storeId,
+        userId,
+      },
+    });
+
+    return NextResponse.json(store);
+  } catch (e) {
+    const error = e as Error;
+    return new NextResponse(error.message, { status: 500 });
+  }
+}
