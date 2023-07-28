08d81ec52d8a2465512060f08b66ae2ceb32a90c
Daniel Junior <oidanieljunior@gmail.com>
2023-07-28 15:15:05 -0300

    fix: query only for featured billboard on store

--- src/app/(public-api)/v1/api/[storeId]/billboards/featured/route.ts
+++ src/app/(public-api)/v1/api/[storeId]/billboards/featured/route.ts
@@ -2,16 +2,23 @@ import { NextResponse } from "next/server";
 
 import { db } from "@/lib/db";
 
-export async function GET(req: Request) {
+export async function GET(
+  req: Request,
+  { params }: { params: { storeId: string } }
+) {
   try {
     const billboard = await db.billboard.findFirst({
       where: {
         isFeatured: true,
+        storeId: params.storeId,
       },
     });
 
     if (!billboard) {
       const newestBillboard = await db.billboard.findFirst({
+        where: {
+          storeId: params.storeId,
+        },
         orderBy: {
           createdAt: "desc",
         },
